1f63b4a3d6128906a30a89e38f8202f8ad755c68
zzz <fengzhiquxiang@gmail.com>
2016-02-22 16:38:39 +0800

    fourth commit

--- src/gopl.io/ch4/struct/main.go
+++ src/gopl.io/ch4/struct/main.go
@@ -16,12 +16,22 @@ type Employee struct {
 	ManagerID int
 }
 
-func main() {
+var emp = make(map[int]*Employee)
 
+func main() {
 	var dilbert Employee
     var thirdvar *Employee = &dilbert
     thirdvar.Position = "china"
+    thirdvar.ID = 999
+    thirdvar.Name = "zzz"
     fmt.Printf("%v\n", thirdvar.Position)
     (*thirdvar).Position = "cccc"
     fmt.Printf("%v\n", (*thirdvar).Position)
+
+    emp[1] = thirdvar
+    fmt.Println(EmployeeByID(1))
+}
+
+func EmployeeByID(id int) *Employee {
+    return emp[id]
 }
